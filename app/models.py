e47ce703efa4e9606b8a23bca7a4a2d4f2d71733
Stas <stas220693@gmail.com>
2021-02-28 15:04:40 +0300

    28-02-21

--- app/models.py
+++ app/models.py
@@ -55,8 +55,8 @@ class Categories(db.Model):
             category_id=id,
             category_name=category_name)
         do_update_stmt = insert_stmt.on_conflict_do_update(
-            constraint='category_name',
-            set_=dict(category_name='updated value'))
+            constraint=category_name,
+            set_=dict(category_name=category_name))
         db.session.execute(do_update_stmt)
         db.session.commit()
         return False
